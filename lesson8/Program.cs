ec977697c3759956e5a321b0017f74cc52b3a46d
Алексей Захаров <zakharov.nnov@gmail.com>
2021-10-16 18:40:44 +0300

    изм. 1

--- lesson8/Program.cs
+++ lesson8/Program.cs
@@ -14,8 +14,25 @@ namespace directory
             //Выберите любую папку на своем компьютере, имеющую вложенные директории.
             //Выведите на консоль ее содержимое и содержимое всех подкаталогов.
 
-            string[] dirs = Directory.GetDirectories("G:\\");
+            string[] dirs = Directory.GetDirectories("G:\\Music");
+            string[] files = Directory.GetFiles("G:\\Music");
+            
             foreach (string s in dirs)
+            {
+                Console.WriteLine(s);
+                string[] dirs2 = Directory.GetDirectories(s);
+                foreach (string s2 in dirs2)
+                {
+                    Console.WriteLine("   {0}",s2);
+                }
+                string[] files2 = Directory.GetFiles(s);
+                foreach (string s2 in files2)
+                {
+                    Console.WriteLine("   {0}",s2);
+                }
+            }
+            Console.WriteLine();
+            foreach (string s in files)
             {
                 Console.WriteLine(s);
             }
